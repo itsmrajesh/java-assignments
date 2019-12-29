b1da908089a339c5d00dd92dab86fac35afeb31a
M Rajesh <m.rajeshrajz@gmail.com>
2019-12-29 15:01:58 +0530

    added delete feature in driver

--- JDBC_Assignment/src/main/java/com/noobs/cj/driver/App.java
+++ JDBC_Assignment/src/main/java/com/noobs/cj/driver/App.java
@@ -14,7 +14,8 @@ public class App {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		while (true) {
-			System.out.println("1.Add Product 2.Delete by ID 3.Update by iD 4.Search by ID 5.View All Products");
+			System.out.println(
+					"1.Add Product 2.Delete by ID 3.Update by iD 4.Search by ID 5.View All Products 6.Delete All Products");
 			int choice = sc.nextInt();
 			switch (choice) {
 			case 1:
@@ -23,7 +24,7 @@ public class App {
 				break;
 			case 2:
 				// 2.Delete by ID
-			 dbDao.deleteProductByid(1);
+				dbDao.deleteProductByid(1);
 				break;
 			case 3:
 				// 3.update by ID
@@ -37,11 +38,13 @@ public class App {
 			case 5:
 				// 5.view all products
 				List<Product> productList = dbDao.viewAllProducts();
-				for(Product p : productList) {
+				for (Product p : productList) {
 					System.out.println(p);
 				}
 				break;
-
+			case 6:
+				dbDao.deleteAllProducts();
+				break;
 			default:
 				System.out.println("Select from 1-5 only");
 				dbDao.deleteAllProducts();
